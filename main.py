cabfa280a982c1395fd744fd7c271fccf1b6f743
Ihtesham <ihtesham.shah90@gmail.com>
2025-08-20 12:34:34 +0200

    added fidelity function in the utils file and in the main file

--- main.py
+++ main.py
@@ -18,13 +18,30 @@ def main():
     ut = Utils(dataset_path="./data/NKR_IKNL_breast_syntheticdata.csv")
     warnings.filterwarnings('ignore')
     data, X, Y, classes_names = ut.data_read_function( )
-    Plot_Data={}
     Treatments=['chemo','target','hormonal','radio','surgery']
+
+    guidline_Plot_Data={}
+    fidelity_Plot_Data={}
+
     for treatment_name in Treatments :
-        Plot_Data= ut.main_function_guidlineComparison(data, X, Y, classes_names, Plot_Data, treatment_name)
+        guidline_Plot_Data= ut.main_function_guidlineComparison(X, Y, classes_names, guidline_Plot_Data)
+        fidelity_Plot_Data= ut.Lime_Shap_fidelity(data, X, Y, classes_names, fidelity_Plot_Data)
 
     colors = ['skyblue', 'orange'] #plot colors for shap and Lime
-    ut.Plot_the_data(Plot_Data, colors, filename = 'guidline_comparison_plot')
+    ut.Plot_the_data(guidline_Plot_Data, colors, filename = 'guidline_comparison_plot')
+
+
+
+
+
+
+
+    
+    
+
+    os.makedirs("results", exist_ok=True)
+    results_df = pd.DataFrame(Plot_Data)
+    results_df.to_csv("GuidlineComparison_plotData.csv", index=False)
 
 
 
